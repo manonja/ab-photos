fc6958ba5a34a2a228d422e5342b4079b8228777
manonja <manon.jacquin@gmail.com>
2024-05-14 16:09:53 +0200

    feat - add about page with text and layout adjustments

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -4,6 +4,8 @@ export const runtime = 'edge';
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
     return (
+        <main className="flex min-h-screen flex-col justify-between items-center p-6">
             <BackgroundImage slug={params.slug}/>
+        </main>
     )
 }
