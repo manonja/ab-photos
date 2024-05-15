a34c38ff1554a4224cb89747f72fd4b3c2a3b82d
manonja <manon.jacquin@gmail.com>
2024-05-15 21:28:34 +0200

    feat - add basic layout for work pages with slug props

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -1,11 +1,13 @@
-import BackgroundImage from "@/app/work/components/backgroundImage";
+import ProjectDetails from "@/app/work/components/projectDetails";
+import ProjectPhotos from "@/app/work/components/projectPhotos";
 
 export const runtime = 'edge';
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
     return (
         <main className="flex min-h-screen flex-col justify-between items-center p-6">
-            <BackgroundImage slug={params.slug}/>
+            <ProjectDetails slug={params.slug}/>
+            <ProjectPhotos slug={params.slug}/>
         </main>
     )
 }
