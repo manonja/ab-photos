443cff56baa3fae6c40e6c513984b17a68f703c9
manonja <manon.jacquin@gmail.com>
2024-05-22 13:39:05 +0200

    feat - cleanup

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,18 +2,6 @@ import SubscribeForm from "@/app/subscribe/components/subscribeForm";
 
 export default function Contact() {
     return (
-        // <main className="flex w-full min-h-screen flex-col items-center p-6">
-        //     <div className="flex w-full relative py-[6%] px-[2%] mx-auto ">
-        //         <div className="flex flex-wrap gap-4 md:flex-nowrap">
-        //             <div className="flex-1 w-full md:flex-initial">
-        //                 <h2 className="uppercase text-2xl font-light">Contact</h2>
-        //                 <div className="my-8 h-px bg-white w-full max-w-full"/>
-        //
-        //                 <text className="text-4xl font-extralight">anton [at] bossenbroek [dot] photo</text>
-        //             </div>
-        //         </div>
-        //     </div>
-        // </main>
     <main className="flex w-full min-h-screen flex-col items-center p-6">
         <div className="flex flex-col relative py-[10%] px-[2%] mx-auto ">
             <h2 className="uppercase text-2xl font-light">Contact</h2>
