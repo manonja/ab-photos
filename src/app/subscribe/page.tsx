8a2df3bc4df06077bd38e70a02406a42f99b6998
manonja <manon.jacquin@gmail.com>
2024-05-22 12:34:07 +0200

    feat - add contact and subscribe pages

---
+++ src/app/subscribe/page.tsx
@@ -0,0 +1,15 @@
+export default function Subscribe() {
+    return (
+        <main className="flex w-full min-h-screen flex-col items-center p-6">
+            <div className="flex w-full relative py-[6%] px-[2%] mx-auto ">
+                <div className="flex flex-wrap gap-4 md:flex-nowrap">
+                    <div className="flex-1 w-full md:flex-initial">
+                        <h2 className="uppercase text-2xl font-light">Subscribe</h2>
+                        <div className="my-8 h-px bg-white w-full max-w-full"/>
+                    </div>
+                </div>
+            </div>
+        </main>
+
+    )
+}
