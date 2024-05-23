7437f1637820c6ffd0153f3003dda614c574dab2
manonja <manon.jacquin@gmail.com>
2024-05-23 22:10:34 +0200

    feat - add complementaty text to seven rad project

--- src/utils/getComplementaryText.ts
+++ src/utils/getComplementaryText.ts
@@ -1,7 +1,7 @@
 export const getComplementaryText = (slug: string): string => {
     let complementaryText;
     if (slug === '7-rad') {
-        complementaryText = ""
+        complementaryText = "After diving into satellite data of the NASA/NOAA VIIRS mission, I confirmed that the Netherlands has the highest amounts of artificial light at night (ALAN) of any place in the world. It also has the highest average ALAN of any OECD countries. Measured in radiance (rad), the satellite measured a peak pollution of 10.500 rad close to a village in the center of the Netherlands1. The average in the Netherlands is 7 rad. ALAN has proven to affect the circadian rhythm and amounts of melatonin in the human body. Changes in melatonin have been linked to increase risks for cancer."
     }
     else if (slug === 'pyrenees') {
         complementaryText = "Traveling with analog photography equipment also reminded us of the importance, rarity, and difficulty of slowing down today. The photographic project, combined with the challenging terrain, forced us to go with the elements. This project aims to inspire and invite the viewer to slow down, and through adventure and nature, to reconnect with the wild and healing force within us."
