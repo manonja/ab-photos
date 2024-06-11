8fd795e6f88b3eaecba08738544daeb3ad8e775e
manonja <manon.jacquin@gmail.com>
2024-06-11 20:06:41 +0200

    feat - decrease margin top on mobile

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default function ProjectPage({ params }: { params: { slug: string } }) {
         <main className="flex min-h-screen flex-col lg:w-[90%] justify-between items-center lg:p-6 p-2">
             <div className="mt-40 lg:pt-0 h-px bg-white w-full"/>
             <ProjectDetails slug={params.slug}/>
-            <div className="mt-32 lg:pt-0 h-px w-full"/>
+            <div className="lg:mt-32 mt-16 lg:pt-0 h-px w-full"/>
             <ProjectPhotos slug={params.slug}/>
         </main>
     )
