6fbf26827d64b52f91728be2bff9ad4ff26f99a9
manonja <manon.jacquin@gmail.com>
2024-06-11 19:08:37 +0200

    feat - add padding bottom on mobile

--- src/app/work/components/projectPhotos.tsx
+++ src/app/work/components/projectPhotos.tsx
@@ -12,7 +12,7 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = async ({slug}) => {
     return (
         <div className="grid text-center p-6 gap-2 lg:mb-0 lg:grid-cols-1 lg:text-right self-end">
             {photos.map((photo) => (
-                <div key={photo.id} className="w-full lg:p-12">
+                <div key={photo.id} className="w-full lg:p-12 pb-2">
                     <img
                         src={photo.desktop_blob}
                         alt={photo.caption || ''}
