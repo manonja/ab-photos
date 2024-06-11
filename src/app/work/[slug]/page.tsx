c76cc3a4d13669778261444c3085a901e5043069
manonja <manon.jacquin@gmail.com>
2024-06-11 20:05:19 +0200

    feat - improve paddng on mobile

--- src/app/work/[slug]/page.tsx
+++ src/app/work/[slug]/page.tsx
@@ -6,7 +6,7 @@ export const runtime = 'edge';
 
 export default function ProjectPage({ params }: { params: { slug: string } }) {
     return (
-        <main className="flex min-h-screen flex-col w-[90%] justify-between items-center lg:p-6 p-2">
+        <main className="flex min-h-screen flex-col lg:w-[90%] justify-between items-center lg:p-6 p-2">
             <div className="mt-40 lg:pt-0 h-px bg-white w-full"/>
             <ProjectDetails slug={params.slug}/>
             <div className="mt-32 lg:pt-0 h-px w-full"/>
