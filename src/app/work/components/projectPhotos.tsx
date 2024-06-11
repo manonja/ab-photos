1b7f7c7afaf4fcf0cb79ae643c427678d9395ced
manonja <manon.jacquin@gmail.com>
2024-06-11 19:59:46 +0200

    feat - remove padding on mobile around photos

--- src/app/work/components/projectPhotos.tsx
+++ src/app/work/components/projectPhotos.tsx
@@ -10,9 +10,9 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = async ({slug}) => {
     const photos = await getPhotoDetails(slug)
 
     return (
-        <div className="grid text-center p-6 gap-2 lg:mb-0 lg:grid-cols-1 lg:text-right self-end">
+        <div className="grid text-center lg:p-6 gap-2 lg:mb-0 lg:grid-cols-1 lg:text-right self-end">
             {photos.map((photo) => (
-                <div key={photo.id} className="w-full lg:p-12 pb-2">
+                <div key={photo.id} className="w-full lg:p-12 pb-4">
                     <img
                         src={photo.desktop_blob}
                         alt={photo.caption || ''}
