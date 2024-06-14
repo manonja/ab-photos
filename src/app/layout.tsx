ea6e9e63c41ff9668d7972b59012e9ca526f365b
manonja <manon.jacquin@gmail.com>
2024-06-14 12:13:45 +0200

    feat - install plausible and add script to head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Lexend } from "next/font/google";
+import PlausibleProvider from 'next-plausible';
+
 
 import "./globals.css";
 import Navbar from "@/components/navbar";
@@ -18,6 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+      <PlausibleProvider domain="bossenbroek.photo" />
+    </head>
     <body className={lexend.className}>
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
       <Navbar/>
