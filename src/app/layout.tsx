d5eddf1a7f3d4a98d1e181a83033107cb5d3f6cf
manonja <manon.jacquin@gmail.com>
2024-06-14 15:31:15 +0200

    feat - add missing files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,10 @@ const lexend = Lexend({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Anton Bossenbroek Photography",
   description: "Anton Bossenbroek photography portfolio",
+  icons: {
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
 };
 
 export default function RootLayout({
@@ -19,8 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
+      <link rel="icon" href="/favicon.ico"/>
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      <link rel="manifest" href="/site.webmanifest"/>
       <script defer data-domain="bossenbroek.photo"
-      src="https://plausible.io/js/script.js"/>
+              src="https://plausible.io/js/script.js"/>
     </head>
     <body className={lexend.className}>
     <main className="flex min-h-screen flex-col items-center justify-between p-6">
