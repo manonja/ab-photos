cf32694b7370d63404ba1f7292a6b07b054df7dd
Manon <manon.jacquin@gmail.com>
2026-01-21 19:48:17 -0800

    Revert "Fix Payload admin readability by scoping site theme"
    This reverts commit 93a42358f51cf9b949e31381e85de4688f76e5dc.

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -6,11 +6,9 @@ export default function SiteLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-neutral-900 text-white">
-      <main className="flex min-h-screen flex-col items-center justify-between p-6">
-        <Navbar />
-        {children}
-      </main>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-6">
+      <Navbar />
+      {children}
+    </main>
   );
 }
